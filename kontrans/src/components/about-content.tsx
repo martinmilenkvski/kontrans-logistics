580e0cc81872812db88f0b92c23aff1aff1adaff
Martin Milenkovski <martinmilex@gmial.com>
2025-01-23 16:32:10 +0100

    About layout adjustment

--- kontrans/src/components/about-content.tsx
+++ kontrans/src/components/about-content.tsx
@@ -30,7 +30,7 @@ const features = [
 
 export function AboutContent() {
   return (
-    <section className="py-24 bg-slate-900">
+    <section className="py-28 bg-slate-900">
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
         <motion.div
           initial={{ opacity: 0, y: 20 }}
