f4e5ab7a4e593e7da1f191782a6c24daf443e834
martinmilenkvski <51443462+martinmilenkvski@users.noreply.github.com>
2025-01-25 16:47:07 +0100

    Login context

---
+++ kontrans/src/context/AuthContext.tsx
@@ -0,0 +1,30 @@
+import { createContext, useContext, useState } from "react";
+
+interface AuthContextType {
+  isLoggedIn: boolean;
+  login: () => void;
+  logout: () => void;
+}
+
+export const AuthContext = createContext<AuthContextType | null>(null);
+
+export function AuthProvider({ children }: { children: React.ReactNode }) {
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+
+  const login = () => setIsLoggedIn(true);
+  const logout = () => setIsLoggedIn(false);
+
+  return (
+    <AuthContext.Provider value={{ isLoggedIn, login, logout }}>
+      {children}
+    </AuthContext.Provider>
+  );
+}
+
+export const useAuth = () => {
+  const context = useContext(AuthContext);
+  if (!context) {
+    throw new Error("Loggin Loggin Loggin Yeah");
+  }
+  return context;
+};
