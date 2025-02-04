fb5914a42b8e8f933d402c33829387f9c9b7f17c
Martin Milenkovski <martinmilex@gmial.com>
2025-02-04 15:48:48 +0100

    Firebase setup

---
+++ kontrans/src/storage/firebaseConfig.ts
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDwJLLMKR6klTmf3GV4Kj0HpEEW2rbcJjM",
+  authDomain: "kontrans-89668.firebaseapp.com",
+  projectId: "kontrans-89668",
+  storageBucket: "kontrans-89668.firebasestorage.app",
+  messagingSenderId: "253142956028",
+  appId: "1:253142956028:web:73b3a624c70a6b3c5ce479",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
